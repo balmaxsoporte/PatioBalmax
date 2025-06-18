4ce1dda15aa99bc91257e6660fc0147c92efe33b
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 08:16:41 -0400

    Update LoginScreen.kt

--- app/src/main/java/com/balmax/patiobalmax/LoginScreen.kt
+++ app/src/main/java/com/balmax/patiobalmax/LoginScreen.kt
@@ -1,10 +1,10 @@
-package com.example.patiobalmax
+package com.balmax.patiobalmax
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.patiobalmax.databinding.LoginScreenBinding
+import com.balmax.patiobalmax.databinding.LoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
 
