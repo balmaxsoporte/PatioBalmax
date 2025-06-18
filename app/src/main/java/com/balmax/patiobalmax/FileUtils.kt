3cd54714fc788aebdc7d042aa92481ef5b538835
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 08:42:20 -0400

    Create FileUtils.kt

---
+++ app/src/main/java/com/balmax/patiobalmax/FileUtils.kt
@@ -0,0 +1,15 @@
+package com.balmax.patiobalmax.utils
+
+import android.content.Context
+
+object FileUtils {
+    fun readLinesFromAsset(context: Context, fileName: String): List<String> {
+        return context.assets.open(fileName).bufferedReader().use { it.readLines() }
+    }
+
+    fun getLicensePlatesFromLines(lines: List<String>): Set<String> {
+        return lines.map { line ->
+            line.split("\"").filter { it.isNotBlank() }.firstOrNull()?.trim()
+        }.filterNotNull().toSet()
+    }
+}
