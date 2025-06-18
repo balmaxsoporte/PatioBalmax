247bf4366fa57ff79916aae8a865889a52abe39c
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 19:05:25 -0400

    Update and rename UserManagement.kt to UserManagement.kt

--- app/src/main/java/com/example/patiobalmax/UserManagement.kt
+++ app/src/main/java/com/example/patiobalmax/UserManagement.kt
@@ -1,8 +1,8 @@
-package com.balmax.patiobalmax
+package com.example.patiobalmax
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.balmax.patiobalmax.databinding.UserManagementBinding
+import com.example.patiobalmax.databinding.UserManagementBinding
 
 class UserManagement : AppCompatActivity() {
 
