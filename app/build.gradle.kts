eac769f86a1f2f97d156c0d68aada28c9f943297
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 10:04:14 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,28 +17,15 @@ subprojects {
     }
 
     android {
-        compileSdk = 33
-        defaultConfig {
-            applicationId = "com.balmax.patiobalmax"
-            minSdk = 29
-            targetSdk = 33
-            versionCode = 1
-            versionName = "1.0"
+    namespace = "com.example.patiobalmax"
+    compileSdk = 34
 
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-        buildTypes {
-            release {
-                isMinifyEnabled = false
-                proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            }
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
+    defaultConfig {
+        applicationId = "com.example.patiobalmax"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
     }
 }
