7bfc307bd94bbb8cc99aa30bd43ebe872391ddbe
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 21:14:02 -0400

    Update UserManagement.kt

--- app/src/main/java/com/example/patiobalmax/UserManagement.kt
+++ app/src/main/java/com/example/patiobalmax/UserManagement.kt
@@ -10,18 +10,9 @@ class UserManagement : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = UserManagementBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.addButton.setOnClickListener {
-            val username = binding.newUsername.text.toString()
-            val password = binding.newPassword.text.toString()
-            val editUsers = binding.editUsersCheck.isChecked
-            val editLots = binding.editLotsCheck.isChecked
-            val editPlates = binding.editPlatesCheck.isChecked
-
-            // Aquí insertar usuario a base de datos o mostrar mensaje
-        }
 
         binding.backButton.setOnClickListener { finish() }
     }
