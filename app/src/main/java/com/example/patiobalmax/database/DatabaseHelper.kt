6906b52769baca078586c2c247dd02c79534f3cb
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 22:31:29 -0400

    Update DatabaseHelper.kt

--- app/src/main/java/com/example/patiobalmax/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/patiobalmax/database/DatabaseHelper.kt
@@ -1,5 +1,16 @@
 package com.example.patiobalmax.database
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.LinearLayout
+import androidx.core.widget.NestedScrollView
+import android.view.LayoutInflater
+import android.view.View
 import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
