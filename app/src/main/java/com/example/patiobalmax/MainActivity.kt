a037248d9a75558ca6e2ebaf864f553f90c571eb
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-18 22:28:29 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/patiobalmax/MainActivity.kt
+++ app/src/main/java/com/example/patiobalmax/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.patiobalmax
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.LinearLayout
+import androidx.core.widget.NestedScrollView
+import android.view.LayoutInflater
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
