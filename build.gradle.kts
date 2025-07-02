0724b55cae74b978336b891d9c69b3335685460a
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-02 19:48:07 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
 // build.gradle.kts (Project-level)
+
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.20"
 }
-val sourceCompatibility by extra(JavaVersion.VERSION_21)
+
+repositories {
+    // Repositorio oficial de Google (donde se encuentran todas las librerías de AndroidX)
+    google()
+    // Repositorio Maven Central (para otras dependencias como Room, Lifecycle, etc.)
+    mavenCentral()
+}
