0efa35f07273556d7822095a57842f45ec52726f
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-02 08:18:26 -0400

    Create MainActivity.kt

---
+++ app/src/main/kotlin/com/example/patiobalmax/ui/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.patiobalmax.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.patiobalmax.MapaEstacionamiento
+
+class MainActivity : AppCompatActivity() {
+    // Similar a LoginActivity, usado como referencia para navegación.
+}
