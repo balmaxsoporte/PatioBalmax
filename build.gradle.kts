4abe9fb7d92161b6facbe179864227024e6bbd2c
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-03 01:29:53 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,24 @@
 // build.gradle.kts (Project-level)
 
 plugins {
-    id("com.android.application") version "8.10.1" apply false
+    id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.20"
 }
 
 repositories {
-    // Repositorio oficial de Google (donde se encuentran todas las librerías de AndroidX)
+    // Repositorio oficial de Google (para AndroidX, Material, AppCompat, etc.)
     google()
-    // Repositorio Maven Central (para otras dependencias como Room, Lifecycle, etc.)
+    // Repositorio Maven Central (para otras bibliotecas como Room, Lifecycle, etc.)
     mavenCentral()
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PatioBalmax"
+include(":app")
