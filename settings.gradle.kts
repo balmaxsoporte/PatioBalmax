ca83be599d6554257d1e7b1bd3031201bc441d63
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-04 04:29:26 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io ") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 include(":app")
