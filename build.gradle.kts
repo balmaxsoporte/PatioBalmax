f0e4f187f3e1f7199fac3f35a30acb807a8b1ef1
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-04 04:32:25 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.11.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
+
+repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io ") }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io ") }
+    }
+}
+
+rootProject.name = "PatioBalmax"
+include(":app")
