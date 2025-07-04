904a18c367000a70ef299f36d08452c81ad31e87
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-04 01:42:44 -0400

    Create AppDatabase.kt

---
+++ app/src/main/kotlin/com/example/patiobalmax/database/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.patiobalmax.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.patiobalmax.database.dao.PuestoDao
+import com.example.patiobalmax.database.dao.UsuarioDao
+import com.example.patiobalmax.database.entity.Puesto
+import com.example.patiobalmax.database.entity.Usuario
+
+@Database(entities = [Usuario::class, Puesto::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun usuarioDao(): UsuarioDao
+    abstract fun puestoDao(): PuestoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "patiobalmax_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
