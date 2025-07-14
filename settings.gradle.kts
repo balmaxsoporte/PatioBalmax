b335166a7580920bd504f517a0f6244f82906a6c
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-14 01:34:53 -0400

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://www.jitpack.io" ) }
-    }
-}
-include(":app")
