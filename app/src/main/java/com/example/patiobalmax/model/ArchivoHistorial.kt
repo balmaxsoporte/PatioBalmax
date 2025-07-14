6cad56e625678e509965412cb50f97fab8ed1be1
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-14 09:35:08 -0400

    Delete app directory

--- app/src/main/java/com/example/patiobalmax/model/ArchivoHistorial.kt
+++
@@ -1,7 +0,0 @@
-package com.example.patiobalmax.model
-
-data class ArchivoHistorial(
-    val nombre: String,
-    val tipo: String, // arrendatarios / particulares
-    val fecha: Date
-)
--- app/src/main/java/com/example/patiobalmax/model/EstadoPuesto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.patiobalmax.model
-
-enum class EstadoPuesto {
-    LIBRE,
-    ARRENDATARIO,
-    PARTICULAR
-}
